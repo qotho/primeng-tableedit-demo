11e5ac42a62cd74db42241c0f392fcf9489184b2
Steve Sarandos <ssarandos@aspex.com>
2020-07-08 08:59:09 -0400

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
-import {Component, ViewChildren, QueryList, ElementRef} from '@angular/core';
-import {Validators,FormControl,FormGroup,FormBuilder} from '@angular/forms';
+import { Component, ViewChildren, QueryList, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import {Validators, FormControl, FormGroup, FormBuilder, FormArray} from '@angular/forms';
 import { CarService } from './carservice';
 import { Car } from './car';
-import { FilterUtils } from 'primeng/utils';
-import { LazyLoadEvent } from 'primeng/api';
-import { SelectItem } from 'primeng/api';
-import {MessageService} from 'primeng/api';
-import { EditableColumn } from 'primeng';
+import { SelectItem, MessageService } from 'primeng/api';
 
 
 @Component({
@@ -15,8 +11,10 @@ import { EditableColumn } from 'primeng';
   styleUrls: [ './app.component.css' ],
   providers: [MessageService]
 })
-export class AppComponent { 
-    carForm: FormGroup;
+export class AppComponent implements OnInit, AfterViewInit {
+    mainForm: FormGroup;
+
+    carForms: FormArray;
 
     cars1: Car[];
 
@@ -32,7 +30,7 @@ export class AppComponent {
 
     newRowAdded = false;
 
-    //@ViewChildren('vin', {read: EditableColumn}) private editableColumns: QueryList<EditableColumn>;
+    // @ViewChildren('vin', {read: EditableColumn}) private editableColumns: QueryList<EditableColumn>;
     @ViewChildren('vin') private editableColumns: QueryList<ElementRef>;
 
     constructor(private fb: FormBuilder, private carService: CarService, private messageService: MessageService) { }
@@ -55,25 +53,24 @@ export class AppComponent {
         ];
 
 
-        this.carForm = this.fb.group({
+        this.mainForm = this.fb.group({
             'vin': new FormControl('', [Validators.required, Validators.minLength(6)]),
             'year': new FormControl('', Validators.required),
             'brand': new FormControl('', Validators.required),
             'color': new FormControl('', [Validators.required]),
-            'sold': new FormControl('', [Validators.required]), 
+            'sold': new FormControl('', [Validators.required]),
             'price' : new FormControl(''),
             'saleDate' : new FormControl('')
-        });
+          },
+          {updateOn: 'blur'}
+        );
     }
 
     ngAfterViewInit() {
       console.log(this.editableColumns);
       this.editableColumns.changes.subscribe(list => {
-          console.log(list);
-          //list.forEach(newCol => console.log(newCol));	
-          if (this.newRowAdded) {
+        if (this.newRowAdded) {
             setTimeout(() => {
-              //list.last.onClick();
               list.last.nativeElement.click();
             });
             this.newRowAdded = false;
@@ -91,17 +88,19 @@ export class AppComponent {
       const car = this.cars1[event.index];
       const patched = {};
       patched[event.field] = car[event.field];
-      this.carForm.patchValue(patched);
+      this.mainForm.patchValue(patched);
+      // this.mainForm.markAsPending();
+      this.mainForm.markAsDirty();
     }
 
     onCellEditComplete(event) {
       const car = this.cars1[this.rowBeingEdited];
       console.log(event);
-      car[event.field] = this.carForm.get(event.field).value;
+      car[event.field] = this.mainForm.get(event.field).value;
 
       if (
         event.originalEvent.key && event.originalEvent.key === 'Enter' &&
-        this.cellBeingEdited == 'color' && this.rowBeingEdited == this.cars1.length - 1
+        this.cellBeingEdited === 'color' && this.rowBeingEdited === this.cars1.length - 1
       ) {
         this.addNewRow();
       }
@@ -111,17 +110,20 @@ export class AppComponent {
     }
 
     addNewRow() {
-        this.cars1.push({"brand": "", "year": 2020, "color": "", "vin": "", "sold": false});
+        this.cars1.push({'brand': '', 'year': 2020, 'color': '', 'vin': '', 'sold': false});
         this.newRowAdded = true;
-        //console.log(this.editableColumns[this.cars1.length - 1]);
-        //this.editableColumns[this.cars1.length - 1].nativeElement.click();
+        // console.log(this.editableColumns[this.cars1.length - 1]);
+        // this.editableColumns[this.cars1.length - 1].nativeElement.click();
+    }
+
+    onDeleteRow(rowIndex: number): void {
+      this.cars1.splice(rowIndex, 1);
     }
 
     onKeyDown(event: KeyboardEvent) {
-      //console.log(event);
       if (
-        event.key === 'Tab' && this.cellBeingEdited == 'color' && 
-        this.rowBeingEdited == this.cars1.length - 1
+        event.key === 'Tab' && this.cellBeingEdited === 'color' &&
+        this.rowBeingEdited === this.cars1.length - 1
       ) {
         this.addNewRow();
       }
